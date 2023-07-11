61377064364ba871a8e900f8a6220a2f77e61ab1
MakoNohana <3082619690@qq.com>
2023-07-11 11:33:47 +0800

    完成测试和用户权限模块

---
+++ blog-springboot/src/main/java/org/cuit/epoch/todolist/todolist.java
@@ -0,0 +1,12 @@
+package org.cuit.epoch.todolist;
+
+/**
+ * @author: momoka
+ * @date: 2023/6/16 0:03
+ * @description:
+ */
+public class todolist {
+    // TODO: 给登录的人自然放上数据库中查询到的权限
+    // TODO: 看一下是怎么查询的用户权限，这里就可以吧权限赋值到session中去
+
+}
